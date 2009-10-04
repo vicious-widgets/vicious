19cde2d659de45b6291b55c44c533b0432a26281
Adrian C. (anrxc) <anrxc@sysphere.org>
2009-10-05 00:11:44 +0200

    mbox: read a 30kb chunk by default

--- mbox.lua
+++ mbox.lua
@@ -17,26 +17,28 @@ module("vicious.mbox")
 
 -- {{{ Mailbox widget type
 local function worker(format, mbox)
-    -- mbox could be huge, get a 15kb chunk from EOF
+    -- mbox could be huge, get a 30kb chunk from EOF
     --  * attachments could be much bigger than this
     local f = io.open(mbox)
-    f:seek("end", -15360)
+    f:seek("end", -30720)
     local txt = f:read("*all")
     f:close()
 
+    -- Default value
+    local subject = "N/A"
+
     -- Find all Subject lines
-    for s in string.gfind(txt, "Subject: ([^\n]*)") do subject = s end
-    if subject then
-        -- Spam sanitize only the last subject
-        subject = helpers.escape(subject)
-
-        -- Don't abuse the wibox, truncate
-        subject = helpers.truncate(subject, 22)
-
-        return {subject}
-    else
-        return {"N/A"}
+    for i in string.gfind(txt, "Subject: ([^\n]*)") do
+       subject = i
     end
+
+    -- Spam sanitize only the last subject
+    subject = helpers.escape(subject)
+
+    -- Don't abuse the wibox, truncate
+    subject = helpers.truncate(subject, 22)
+
+    return {subject}
 end
 -- }}}
 
