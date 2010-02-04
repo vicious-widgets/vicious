91925e601d9809bf17e6a79cfa4cfad9937ed29a
Adrian C. (anrxc) <anrxc@sysphere.org>
2010-02-04 18:36:27 +0100

    pkg: pacman moved to generic packages module

--- pkg.lua
+++ pkg.lua
@@ -7,23 +7,22 @@
 local tonumber = tonumber
 local io = { popen = io.popen }
 local setmetatable = setmetatable
---local string = { match = string.match }
 -- }}}
 
 
--- Pacman: provides number of pending updates on Arch Linux
-module("vicious.pacman")
+-- Pkg: provides number of pending updates on GNU/Linux
+module("vicious.pkg")
 
 
--- {{{ Pacman widget type
+-- {{{ Packages widget type
 local function worker(format)
     -- Initialise counters
     local updates = 0
 
-    -- Check if updates are available
+    -- Check if updates are available on Arch
     local f = io.popen("pacman -Qu")
-    -- Exclude IgnorePkg and count deps
-    --local f = io.popen("pacman -Sup")
+    --- Exclude IgnorePkg and count deps
+    ---local f = io.popen("pacman -Sup")
 
     for line in f:lines() do
         updates = updates + 1
