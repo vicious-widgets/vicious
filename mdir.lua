4664beeda1e934e5a01848129c41c969b8ece0ba
Adrian C. (anrxc) <anrxc@sysphere.org>
2010-03-02 23:19:44 +0100

    mdir: fix module description

--- mdir.lua
+++ mdir.lua
@@ -10,7 +10,7 @@ local setmetatable = setmetatable
 -- }}}
 
 
--- Mdir: provides a number of new and unread messages in a Maildir structure
+-- Mdir: provides the number of new and unread messages in Maildir structures/dirs
 module("vicious.mdir")
 
 
