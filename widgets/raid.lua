a9347ec0d033aa08d77fcc75c35d88514f9e84f5
Adrian C. (anrxc) <anrxc@sysphere.org>
2010-03-26 00:59:01 +0100

    raid: import raid state widget type by Hagen
    This widget type returns 1st value as the number of assigned, and 2nd
    as active, devices in the array provided as the widget argument.

---
+++ widgets/raid.lua
@@ -0,0 +1,56 @@
+-----------------------------------------------------
+-- Licensed under the GNU General Public License v2
+--  * (c) 2010, Hagen Schink <troja84@googlemail.com>
+-----------------------------------------------------
+
+-- {{{ Grab environment
+local io = { lines = io.lines }
+local setmetatable = setmetatable
+local string = {
+    len = string.len,
+    sub = string.sub,
+    match = string.match,
+    gmatch = string.gmatch
+}
+-- }}}
+
+
+-- Raid: provides state information for a requested RAID array
+module("vicious.widgets.raid")
+
+
+-- {{{ RAID widget type
+local function worker(format, warg)
+    if not warg then return end
+
+    local found = false
+    local mddev = {}
+    mddev[warg] = {
+        ["active"]   = 0,
+        ["assigned"] = 0
+    }
+
+    -- Linux manual page: md(4)
+    for line in io.lines("/proc/mdstat") do
+        if found then
+            local updev = string.match(line, "%[[_U]+%]")
+
+            for i in string.gmatch(updev, "U") do
+                mddev[warg]["active"] = mddev[warg]["active"] + 1
+            end
+
+            break
+        elseif string.sub(line, 1, string.len(warg)) == warg then
+            found = true
+
+            for i in string.gmatch(line, "%[%d%]") do
+                mddev[warg]["assigned"] = mddev[warg]["assigned"] + 1
+            end
+        end
+    end
+
+    return {mddev[warg]["assigned"], mddev[warg]["active"]}
+end
+-- }}}
+
+setmetatable(_M, { __call = function(_, ...) return worker(...) end })
