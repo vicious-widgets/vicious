4113d371d17087887f2a9a0ae51a20dd4dd3cadf
Adrian C. (anrxc) <anrxc@sysphere.org>
2010-08-22 14:46:05 +0200

    pkg: include FreeBSD support

--- widgets/pkg.lua
+++ widgets/pkg.lua
@@ -26,6 +26,7 @@ local function worker(format, warg)
         ["Debian"] = { cmd = "apt-show-versions -u -b" },
         ["Ubuntu"] = { cmd = "aptitude search '~U'" },
         ["Fedora"] = { cmd = "yum list updates", sub = 3 },
+        ["FreeBSD"] ={ cmd = "pkg_version -I -l '<'" },
         ["Mandriva"]={ cmd = "urpmq --auto-select" }
     }
 
