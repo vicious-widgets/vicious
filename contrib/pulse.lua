25b375bd6b8ee5ceb04d09fca2d8ba413850d278
Joerg T. (Mic92) <jthalheim@gmail.com>
2011-09-01 16:08:02 +0200

    [pulse] round volume to integer

--- contrib/pulse.lua
+++ contrib/pulse.lua
@@ -17,6 +17,9 @@ local string = {
     format = string.format,
     gmatch = string.gmatch
 }
+local math = {
+	floor = math.floor
+}
 -- }}}
 
 
@@ -71,7 +74,7 @@ local function worker(format, sink)
     local vol = tonumber(string.match(data, "set%-sink%-volume "..escape(sink).." (0x[%x]+)"))
     if vol == nil then vol = 0 end
 
-    return { vol/0x10000*100, "on"}
+    return { math.floor(vol/0x10000*100), "on"}
 end
 -- }}}
 
