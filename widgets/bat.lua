vicious-widgets/vicious

commit 350e9240ea792446efb22564060384f535ac4e34

bat: another workaround for broken ACPI implementations

widgets/bat.lua
@@ -73,8 +73,9 @@ local function worker(format, warg)
         else
             return {state, percent, time}
         end
-        local hoursleft = math.floor(timeleft)
-        local minutesleft = math.floor((timeleft - hoursleft) * 60 )
+        -- Calculate time (but work around broken BAT/ACPI implementations)
+        local hoursleft = math.min(math.floor(timeleft), 0)
+        local minutesleft = math.min(math.floor((timeleft - hoursleft) * 60 ), 0)
         time = string.format("%02d:%02d", hoursleft, minutesleft)
     end
 

commit 45c6ebaeb685ba4ed402353ec3de95719d0c6a7e

bat: fix time calculation, bug introduced in 350e924

widgets/bat.lua
@@ -10,6 +10,7 @@ local string = { format = string.format }
 local helpers = require("vicious.helpers")
 local math = {
     min = math.min,
+    max = math.max,
     floor = math.floor
 }
 -- }}}
@@ -65,6 +66,7 @@ local function worker(format, warg)
 
     -- Calculate remaining (charging or discharging) time
     local time = "N/A"
+
     if tonumber(rate) then
         if state == "+" then
             timeleft = (tonumber(capacity) - tonumber(remaining)) / tonumber(rate)
@@ -73,9 +75,11 @@ local function worker(format, warg)
         else
             return {state, percent, time}
         end
+
         -- Calculate time (but work around broken BAT/ACPI implementations)
-        local hoursleft = math.min(math.floor(timeleft), 0)
-        local minutesleft = math.min(math.floor((timeleft - hoursleft) * 60 ), 0)
+        local hoursleft   = math.max(math.floor(timeleft), 0)
+        local minutesleft = math.max(math.floor((timeleft - hoursleft) * 60 ), 0)
+
         time = string.format("%02d:%02d", hoursleft, minutesleft)
     end
 