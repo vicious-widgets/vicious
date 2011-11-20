vicious-widgets/vicious

commit 78e3d37fb5575312233ed7dc11b7d7229c82c0db

Some laptops such as Acer timeline cause time remaining to be a massive negative number when charging.

widgets/bat.lua
@@ -73,8 +73,8 @@ local function worker(format, warg)
         else
             return {state, percent, time}
         end
-        local hoursleft = math.floor(timeleft)
-        local minutesleft = math.floor((timeleft - hoursleft) * 60 )
+        local hoursleft = math.min(math.floor(timeleft), 0)
+        local minutesleft = math.min(math.floor((timeleft - hoursleft) * 60 ), 0)
         time = string.format("%02d:%02d", hoursleft, minutesleft)
     end
 

commit 5c7df93baa5b2284fe93f27beca085b0aa1da8ab

[bat] Fix bug introduced in 78e3d37fb55
Instead of setting the remaining time to reasonable values, remaining
time was always set to 0, because of math.min().

widgets/bat.lua
@@ -74,8 +74,11 @@ local function worker(format, warg)
             return {state, percent, time}
         end
         -- Calculate time (but work around broken BAT/ACPI implementations)
-        local hoursleft = math.min(math.floor(timeleft), 0)
-        local minutesleft = math.min(math.floor((timeleft - hoursleft) * 60 ), 0)
+        local hoursleft, minutesleft = 0, 0
+        if timeleft > 0 then
+            hoursleft = math.floor(timeleft)
+            minutesleft = math.floor((timeleft - hoursleft) * 60 )
+        end
         time = string.format("%02d:%02d", hoursleft, minutesleft)
     end
 