b11bb78e04b1efa1cd7b352f0522c88280647ad4
Adrian C. (anrxc) <anrxc@sysphere.org>
2012-02-05 19:38:22 +0100

    hddtemp: fix curl hang in version 7.24.0

--- widgets/hddtemp.lua
+++ widgets/hddtemp.lua
@@ -21,7 +21,7 @@ local function worker(format, warg)
     if warg == nil then warg = 7634 end
 
     local hdd_temp = {} -- Get info from the hddtemp daemon
-    local f = io.popen("curl --connect-timeout 1 -fsm 3 telnet://127.0.0.1:"..warg)
+    local f = io.popen("echo | curl --connect-timeout 1 -fsm 3 telnet://127.0.0.1:"..warg)
 
     for line in f:lines() do
         for d, t in string.gmatch(line, "|([%/%a%d]+)|.-|([%d]+)|[CF]+|") do
