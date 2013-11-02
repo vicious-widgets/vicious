563cb6f5e85c50771cc859b02970148d5c416efb
Adrian C. (anrxc) <anrxc@sysphere.org>
2013-11-02 18:14:07 +0100

    pkg: revert to pacman as default Arch method, checkupdates optional
    Checkupdates is a good tool avoiding any need for period pacman -Sy
    jobs from root crontab, however it works by maintaining its own
    database in /tmp by default (and _that_ is a tmpfs by default) so on
    every initialization it would stall awesome until it makes the first
    sync. Better approach would be to keep its db inside $HOME for
    persistence, and then to initiate the first (and longest) sync
    manually before converting your package widgets to "Arch C".
    
    All this is the reason I can't recommend it as the default right
    now...

--- widgets/pkg.lua
+++ widgets/pkg.lua
@@ -22,7 +22,8 @@ local function worker(format, warg)
     -- Initialize counters
     local updates = 0
     local manager = {
-        ["Arch"]   = { cmd = "checkupdates" },
+        ["Arch"]   = { cmd = "pacman -Qu" },
+        ["Arch C"] = { cmd = "checkupdates" },
         ["Arch S"] = { cmd = "yes | pacman -Sup", sub = 1 },
         ["Debian"] = { cmd = "apt-show-versions -u -b" },
         ["Ubuntu"] = { cmd = "aptitude search '~U'" },
