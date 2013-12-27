vicious-widgets/vicious

commit dc556e5415ee1d1b3b74508e016fb1693d55a311

bat: Use a real minus sign for the charging status
Previously, a simple hyphen (“-”) was used to show that the battery is
discharging. The minus sign is a distinct char, and also distinct from
the en-dash (“–”) and the em-dash (“—”).

With this commit, I put in a unicode “MINUS SIGN” (0x2212) which I
created with a Digraph in Vim `^k-2`.

Signed-off-by: Adrian C. (anrxc) <anrxc@sysphere.org>

widgets/bat.lua
@@ -30,7 +30,7 @@ local function worker(format, warg)
         ["Unknown\n"]     = "⌁",
         ["Charged\n"]     = "↯",
         ["Charging\n"]    = "+",
-        ["Discharging\n"] = "-"
+        ["Discharging\n"] = "−"
     }
 
     -- Check if the battery is present

commit 6e16a412efc190e450b14dd5f53261e33ec99379

bat: fix broken battery remaining time (was always N/A)
Bug was introduced in commit dc556e5415 and the new discharging
symbol.

Signed-off-by: Adrian C. (anrxc) <anrxc@sysphere.org>

widgets/bat.lua
@@ -74,7 +74,7 @@ local function worker(format, warg)
     if rate ~= nil and rate ~= 0 then
         if state == "+" then
             timeleft = (tonumber(capacity) - tonumber(remaining)) / tonumber(rate)
-        elseif state == "-" then
+        elseif state == "−" then
             timeleft = tonumber(remaining) / tonumber(rate)
         else
             return {state, percent, time, wear}