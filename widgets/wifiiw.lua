84b035019f842093f81e03ce0989c52695330d73
Mellich <mellich@gmx.net>
2016-10-22 22:32:55 +0200

    performed suggested changes in widgets/wifiiw.lua

--- widgets/wifiiw.lua
+++ widgets/wifiiw.lua
@@ -1,11 +1,10 @@
 ---------------------------------------------------
 -- Licensed under the GNU General Public License v2
---  * (c) 2010, Adrian C. <anrxc@sysphere.org>
+--  * (c) 2016, Marius M. <mellich@gmx.net>
 ---------------------------------------------------
 
 -- {{{ Grab environment
 local tonumber = tonumber
-local math = { ceil = math.ceil }
 local setmetatable = setmetatable
 local helpers = require("vicious.helpers")
 local io = {
@@ -24,42 +23,17 @@ local string = {
 local wifiiw = {}
 
 
--- {{{ Variable definitions
-local iw = "iw"
-local iwcpaths = { "/sbin", "/usr/sbin", "/usr/local/sbin", "/usr/bin" }
--- }}}
-
-
 -- {{{ Wireless widget type
 local function worker(format, warg)
     if not warg then return end
 
     -- Default values
-    local winfo = {
-        ["{ssid}"] = "N/A",
-        ["{mode}"] = "N/A",
-        ["{chan}"] = 0,
-        ["{rate}"] = 0,
-        ["{freq}"] = 0,
-		["{txpw}"] = 0,
-		["{linp}"] = 0,
-        ["{sign}"] = 0
-    }
-
-    -- Sbin paths aren't in user PATH, search for the binary
-    if iw == "iw" then
-        for _, p in ipairs(iwcpaths) do
-            local f = io.open(p .. "/iw", "rb")
-            if f then
-                iw = p .. "/iw"
-                f:close()
-                break
-            end
-        end
-    end
+    local winfo = {}
 
     -- Get data from iw where available
-    local f = io.popen(iw .." dev ".. warg .. " link 2>&1;" .. iw .." dev ".. warg .. " info 2>&1")
+    local f = io.popen("export PATH=$PATH:/sbin/:/usr/sbin:/usr/local/sbin;" ..
+					"iw dev ".. helpers.shellquote(tostring(warg)) .. " link 2>&1;" ..
+					"iw dev ".. helpers.shellquote(tostring(warg)) .. " info 2>&1")
     local iwresult = f:read("*all")
     f:close()
 
@@ -69,21 +43,21 @@ local function worker(format, warg)
     end
 	-- string match is simple in most cases, because iw uses a new line for every info
     winfo["{ssid}"] =  -- SSID can have almost anything in it until new line
-      string.match(iwresult, 'SSID: ([^\n]*)') or winfo["{ssid}"]
+      string.match(iwresult, "SSID: ([^\n]*)") or "N/A"
     winfo["{mode}"] =  -- everything after 'type ' until new line
-      string.match(iwresult, "type ([^\n]*)") or winfo["{mode}"]
+      string.match(iwresult, "type ([^\n]*)") or "N/A"
     winfo["{chan}"] =  -- Channels are plain digits
-      tonumber(string.match(iwresult, "channel ([%d]+)") or winfo["{chan}"])
+      tonumber(string.match(iwresult, "channel ([%d]+)") or 0)
     winfo["{rate}"] =  -- We don't want to display Mb/s
-      tonumber(string.match(iwresult, "tx bitrate: ([%d%.]*)") or winfo["{rate}"])
+      tonumber(string.match(iwresult, "tx bitrate: ([%d%.]*)") or 0)
     winfo["{freq}"] =  -- Frequency are plain digits
-      tonumber(string.match(iwresult, "freq: ([%d]+)") or winfo["{link}"])
+      tonumber(string.match(iwresult, "freq: ([%d]+)") or 0)
     winfo["{sign}"] =  -- Signal level can be a negative value, don't display decibel notation
-      tonumber(string.match(iwresult, "signal: (%-[%d]+)") or winfo["{sign}"])
+      tonumber(string.match(iwresult, "signal: (%-[%d]+)") or 0)
     winfo["{linp}"] =  -- Link Quality using the Windows definition (-50dBm->100%, -100dBm->0%)
-      100 - ((winfo["{sign}"] * -2) - 100)
+      (winfo["{sign}"] ~= 0 and 100 - ((winfo["{sign}"] * -2) - 100) or 0)
     winfo["{txpw}"] =  -- TX Power can be a negative value, don't display decibel notation
-      tonumber(string.match(iwresult, "txpower ([%-]?[%d]+)") or winfo["{txpw}"])
+      tonumber(string.match(iwresult, "txpower ([%-]?[%d]+)") or 0)
 
     return winfo
 end
