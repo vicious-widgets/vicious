23c4e8de81bd7b5c10f37bbe6a07133f9af7dd29
mutlusun <mutlusun@github.com>
2017-01-04 13:05:12 +0100

    fixed wrong calculations in cpu_freebsd

--- widgets/cpu_freebsd.lua
+++ widgets/cpu_freebsd.lua
@@ -18,43 +18,58 @@ local string = { gmatch = string.gmatch }
 -- vicious.widgets.cpu_freebsd
 local cpu_freebsd = {}
 
+-- Initialize function tables
+local cpu_total = {}
+local cpu_idle = {}
 
 -- {{{ CPU widget type
 local function worker(format)
-    local kern = helpers.sysctl_table("kern")
-    local cpu_total = {}
-    local cpu_idle = {}
-    local cpu_pusage = {}
-    local i = 0
+    local cp_times = helpers.sysctl("kern.cp_times")
+    local matches = {}
+    local tmp_total = {}
+    local tmp_idle = {}
+    local tmp_usage = {}
 
-    -- CPU usage over all cpus
-    cpu_total[0] = 0
-    for v in string.gmatch(kern.cp_time, "([%d]+)") do
-        cpu_total[0] = cpu_total[0] + tonumber(v)
-        cpu_idle[0] = tonumber(v)
+    -- Read input data
+    for v in string.gmatch(cp_times, "([%d]+)") do
+        table.insert(matches, v)
     end
 
-    i = 0
-    for v in string.gmatch(kern.cp_times, "([%d]+)") do
-        local index = math.floor(i/5) + 1
-
-        if i % 5 == 0 then
-            cpu_total[index] = 0
-        elseif i % 5 == 4 then
-            cpu_idle[index] = tonumber(v)
+    -- Set first value of function tables
+    if #cpu_total == 0 then  -- check for empty table
+        for i = 1, #matches / 5 + 1 do
+            cpu_total[i] = 0
+            cpu_idle[i] = 0
         end
-
-        cpu_total[index] = cpu_total[index] + tonumber(v)
-
-        i = i + 1
+    end
+    for i = 1, #matches / 5 + 1 do
+        tmp_total[i] = 0
+        tmp_idle[i] = 0
+        tmp_usage[i] = 0
     end
 
-    for i = 0, #cpu_total do
-        print((cpu_total[i] - cpu_idle[i])/cpu_total[i])
-        cpu_pusage[i + 1] = math.floor((cpu_total[i] - cpu_idle[i])/cpu_total[i] * 100)
+    -- CPU usage 
+    for i, v in ipairs(matches) do
+        local index = math.floor((i-1) / 5) + 2 -- current cpu
+
+        tmp_total[1] = tmp_total[1] + v
+        tmp_total[index] = tmp_total[index] + v
+
+        if (i-1) % 5 == 4 then
+            tmp_idle[1] = tmp_idle[1] + v
+            tmp_idle[index] = tmp_idle[index] + v
+        end
     end
 
-    return cpu_pusage
+    for i = 1, #tmp_usage do
+        tmp_usage[i] = tmp_total[i] - cpu_total[i]
+        tmp_usage[i] = math.floor((tmp_usage[i] - (tmp_idle[i] - cpu_idle[i])) / tmp_usage[i] * 100)
+    end
+
+    cpu_total = tmp_total
+    cpu_idle = tmp_idle
+
+    return tmp_usage
 end
 -- }}}
 
