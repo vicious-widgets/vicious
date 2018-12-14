c14df133e1edb980a10e78cdf665a7c9480359a0
Nguyễn Gia Phong <vn.mcsinyx@gmail.com>
2018-12-14 17:26:36 +0700

    Make mpd widget type expose more info
    By default, format time values in [hh:]mm:ss. Also use printf instead of
    echo, which is shell-specific.

--- helpers.lua
+++ helpers.lua
@@ -136,6 +136,25 @@ function helpers.uformat(array, key, value, unit)
 end
 -- }}}
 
+-- {{{ Format playing progress
+function helpers.format_progress(elapsed, duration)
+    local em, es = elapsed / 60, elapsed % 60
+    local dm, ds = duration / 60, duration % 60
+
+    if dm < 10 then
+        return ("%d:%02d"):format(em, es), ("%d:%02d"):format(dm, ds)
+    elseif dm < 60 then
+        return ("%02d:%02d"):format(em, es), ("%02d:%02d"):format(dm, ds)
+    elseif dm < 600 then
+        return ("%d:%02d:%02d"):format(em / 60, em % 60, es),
+               ("%d:%02d:%02d"):format(dm / 60, dm % 60, ds)
+    else
+        return ("%02d:%02d:%02d"):format(em / 60, em % 60, es),
+               ("%02d:%02d:%02d"):format(dm / 60, dm % 60, ds)
+    end
+end
+-- }}}
+
 -- {{{ Escape a string
 function helpers.escape(text)
     local xml_entities = {
