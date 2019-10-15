ce66335081d156f007a21220113d287962971fe6
Enric Morales <me@enric.me>
2019-10-15 15:06:02 +0200

    Style code to be more coherent with other modules
    Ident code within the sysctl function scope, add a comment that was
    needed and stop recycling a variable.

--- widgets/bat_openbsd.lua
+++ widgets/bat_openbsd.lua
@@ -29,60 +29,61 @@ local helpers = require("vicious.helpers")
 
 local bat_openbsd = {}
 function bat_openbsd.async(format, warg, callback)
-    local battery = {
-        charging_rate = "hw.sensors.acpi%s.power0",
-        last_full_capacity = "hw.sensors.acpi%s.watthour0",
-        remaining_capacity = "hw.sensors.acpi%s.watthour3",
-        design_capacity = "hw.sensors.acpi%s.watthour4",
-        state = "hw.sensors.acpi%s.raw0",
+    local battery_id = warg or "bat0"
+
+    local fields = {
+	charging_rate = ("hw.sensors.acpi%s.power0"):format(battery_id),
+	last_full_capacity = ("hw.sensors.acpi%s.watthour0"):format(battery_id),
+	remaining_capacity = ("hw.sensors.acpi%s.watthour3"):format(battery_id),
+	design_capacity = ("hw.sensors.acpi%s.watthour4"):format(battery_id),
+	state = ("hw.sensors.acpi%s.raw0"):format(battery_id)
     }
 
-    local sysctl_vars = {}
-    for k, v in pairs(battery) do
-        v = v:format(warg or "bat0")
-        battery[k], sysctl_vars[#sysctl_vars+1] = v, v
-    end
+    local sysctl_args = {}
+    for _, v in pairs(fields) do table.insert(sysctl_args, v) end
 
-    helpers.sysctl_async(sysctl_vars, function(ret)
-    for k, v in pairs(battery) do
-        battery[k] = tonumber(ret[v]:match("(.-) "))
-    end
+    local battery = {}
+    helpers.sysctl_async(sysctl_args, function(ret)
+	    for k, v in pairs(fields) do
+		-- discard the description that comes after the values
+		battery[k] = tonumber(ret[v]:match("(.-) "))
+	    end
 
-    local states = {
-        [0] = "↯",      -- not charging
-        [1] = "-",      -- discharging
-        [2] = "!",      -- critical
-        [3] = "+",      -- charging
-        [4] = "N/A",    -- unknown status
-        [255] = "N/A"   -- unimplemented by the driver
-    }
-    local state = states[battery.state]
+	    local states = {
+		[0] = "↯",      -- not charging
+		[1] = "-",      -- discharging
+		[2] = "!",      -- critical
+		[3] = "+",      -- charging
+		[4] = "N/A",    -- unknown status
+		[255] = "N/A"   -- unimplemented by the driver
+	    }
+	    local state = states[battery.state]
 
-    local charge = tonumber(battery.remaining_capacity
-                                    / battery.last_full_capacity * 100)
+	    local charge = tonumber(battery.remaining_capacity
+				    / battery.last_full_capacity * 100)
 
-    local remaining_time
-    if battery.charging_rate < 1 then
-        remaining_time = "∞"
-    else
-        local raw_time = battery.remaining_capacity / battery.rate
-        local hours, hour_fraction = math.modf(raw_time)
-        local minutes = math.floor(60 * hour_fraction)
-        remaining_time = ("%d:%0.2d"):format(hours, minutes)
-    end
+	    local remaining_time
+	    if battery.charging_rate < 1 then
+		remaining_time = "∞"
+	    else
+		local raw_time = battery.remaining_capacity / battery.rate
+		local hours, hour_fraction = math.modf(raw_time)
+		local minutes = math.floor(60 * hour_fraction)
+		remaining_time = ("%d:%0.2d"):format(hours, minutes)
+	    end
 
-    local wear = math.floor(battery.last_full_capacity,
-                            battery.design_capacity)
+	    local wear = math.floor(battery.last_full_capacity,
+				    battery.design_capacity)
 
-    -- Pass the following arguments to callback function:
-    --  * battery state symbol (↯, -, !, + or N/A)
-    --  * remaining capacity (in percent)
-    --  * remaining time, as reported by the battery
-    --  * wear level (in percent)
-    --  * present_rate (in Watts/hour)
-    return callback({state, charge, remaining_time,
-                        wear, battery.charging_rate})
-    end)
+	    -- Pass the following arguments to callback function:
+	    --  * battery state symbol (↯, -, !, + or N/A)
+	    --  * remaining capacity (in percent)
+	    --  * remaining time, as reported by the battery
+	    --  * wear level (in percent)
+	    --  * present_rate (in Watts/hour)
+	    return callback({ state, charge, remaining_time,
+			    wear, battery.charging_rate })
+	end)
 end
 
 return helpers.setasyncall(bat_openbsd)
