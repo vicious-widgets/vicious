fff9122467a6a34200f05404959bc6681afa8c9c
Nguyễn Gia Phong <cnx@loang.net>
2024-11-12 13:41:10 +0900

    Fix Sphinx extlinks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ release = '2.7.1'
 # They can be extensions coming with Sphinx (named 'sphinx.ext.*')
 # or your custom ones.
 extensions = ['sphinx.ext.extlinks', 'sphinxcontrib.luadomain']
-extlinks = {'github': ('https://github.com/%s', '@')}
+extlinks = {'github': ('https://github.com/%s', '@%s')}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
